e8de19753508d4c32eed18c33e5fb52def424748
choimyungbin <71762087+choimyungbin@users.noreply.github.com>
2022-03-09 22:55:32 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,7 @@
+#첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+a,b=input().split()
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(int(a)/int(b)))
+print(int(a)%int(b))
