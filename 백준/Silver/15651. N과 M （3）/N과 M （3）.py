9f8be1668424638e77835700bd2d82a040409f3b
최명빈 <71762087+choimyungbin@users.noreply.github.com>
2022-05-02 23:20:54 +0900

    [Silver III] Title: N과 M (3), Time: 624 ms, Memory: 122856 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,13 +1,13 @@
 n, m = map(int, input().split())
 stack = []
-def backtrack(qlength):
-    if qlength == m:
+def backtrack(slength):
+    if slength == m:
         print(' '.join(map(str,stack)))
         return
     else:
         for i in range(1, n+1):
             stack.append(i)
-            backtrack(len(stack))
+            backtrack(slength+1)
             stack.pop()
 
 backtrack(0)
